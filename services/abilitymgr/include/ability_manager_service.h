a86e03662ee3012f4eaaf70b4bf7e514c9555d94
ohos-lsw <lishiwei6@huawei.com>
2021-09-28 00:22:36 +0800

    fixed 5e60102 from https://gitee.com/ohos-lsw/aafwk_standard/pulls/93 Start phone service
    Signed-off-by: ohos-lsw <lishiwei6@huawei.com>

--- services/abilitymgr/include/ability_manager_service.h
+++ services/abilitymgr/include/ability_manager_service.h
@@ -576,6 +576,12 @@ private:
     void StartingLauncherAbility();
 
     /**
+     * starting phone service ability.
+     *
+     */
+    void StartingPhoneServiceAbility();
+
+    /**
      * starting system ui abilites.
      *
      */
