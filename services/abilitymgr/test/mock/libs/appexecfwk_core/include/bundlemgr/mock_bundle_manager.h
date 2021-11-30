be3f5d53a667ae6545eee8aeff8cb8aba73a07e2
zhaoyuan17 <zhaoyuan17@huawei.com>
2021-11-30 13:04:56 +0000

    Fix code
    Signed-off-by: zhaoyuan17 <zhaoyuan17@huawei.com>

--- services/abilitymgr/test/mock/libs/appexecfwk_core/include/bundlemgr/mock_bundle_manager.h
+++ services/abilitymgr/test/mock/libs/appexecfwk_core/include/bundlemgr/mock_bundle_manager.h
@@ -199,7 +199,8 @@ public:
     MOCK_METHOD3(GetFormsInfoByModule,
         bool(const std::string &bundleName, const std::string &moduleName, std::vector<FormInfo> &formInfos));
     MOCK_METHOD2(GetShortcutInfos, bool(const std::string &bundleName, std::vector<ShortcutInfo> &shortcutInfos));
-    MOCK_METHOD2(GetAllCommonEventInfo, bool(const std::string &eventKey, std::vector<CommonEventInfo> &commonEventInfos));
+    MOCK_METHOD2(GetAllCommonEventInfo, bool(const std::string &eventKey,
+        std::vector<CommonEventInfo> &commonEventInfos));
 };
 
 class BundleMgrStub : public IRemoteStub<IBundleMgr> {
@@ -278,7 +279,8 @@ public:
     MOCK_METHOD3(GetFormsInfoByModule,
         bool(const std::string &bundleName, const std::string &moduleName, std::vector<FormInfo> &formInfos));
     MOCK_METHOD2(GetShortcutInfos, bool(const std::string &bundleName, std::vector<ShortcutInfo> &shortcutInfos));
-    MOCK_METHOD2(GetAllCommonEventInfo, bool(const std::string &eventKey, std::vector<CommonEventInfo> &commonEventInfos));
+    MOCK_METHOD2(GetAllCommonEventInfo, bool(const std::string &eventKey,
+        std::vector<CommonEventInfo> &commonEventInfos));
     MOCK_METHOD0(UnregisterBundleStatusCallback, bool());
     MOCK_METHOD3(DumpInfos, bool(const DumpFlag flag, const std::string &bundleName, std::string &result));
     MOCK_METHOD1(IsApplicationEnabled, bool(const std::string &bundleName));
