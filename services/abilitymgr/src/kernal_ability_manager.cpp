ec36c845f76b32630355e691d1250fd7336f7b58
xuchenghua <xuchenghua09@huawei.com>
2022-01-11 12:45:57 +0000

    xuchenghua09@huawei.com
    Signed-off-by: xuchenghua <xuchenghua09@huawei.com>

--- services/abilitymgr/src/kernal_ability_manager.cpp
+++ services/abilitymgr/src/kernal_ability_manager.cpp
@@ -200,6 +200,7 @@ void KernalAbilityManager::GetOrCreateAbilityRecord(
         return;
     }
     targetAbility = AbilityRecord::CreateAbilityRecord(abilityRequest);
+    targetAbility->SetUseNewMission();
     abilities_.push_front(targetAbility);
 }
 
