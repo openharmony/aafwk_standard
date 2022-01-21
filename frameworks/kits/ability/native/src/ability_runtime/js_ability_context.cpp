5d5dfec29a4b30e434d7ddc4421778ca75d43cfe
mingxihua <mingxihua@huawei.com>
2022-01-21 02:41:37 +0000

    IssueNo:#I4PCM1:ce/de context Description: ce/de context Sig:SIG_ApplicationFramework Feature or Bugfix:Feature Binary Source:No
    Signed-off-by: mingxihua <mingxihua@huawei.com>

--- frameworks/kits/ability/native/src/ability_runtime/js_ability_context.cpp
+++ frameworks/kits/ability/native/src/ability_runtime/js_ability_context.cpp
@@ -327,8 +327,12 @@ NativeValue* JsAbilityContext::OnDisconnectAbility(NativeEngine& engine, NativeC
                 return;
             }
             HILOG_INFO("context->DisconnectAbility");
-            context->DisconnectAbility(want, connection);
-            task.Resolve(engine, engine.CreateUndefined());
+            auto errcode = context->DisconnectAbility(want, connection);
+            if (errcode == 0) {
+                task.Resolve(engine, engine.CreateUndefined());    
+            } else {
+                task.Resolve(engine, CreateJsError(engine, errcode, "Disconnect Ability failed."));
+            }
         };
 
     NativeValue* lastParam = (info.argc == ARGC_ONE) ? nullptr : info.argv[1];
