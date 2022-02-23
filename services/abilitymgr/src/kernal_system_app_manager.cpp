908a7c7846746482f9a8a7b725c84c5344f1eb14
unknown <sijunjie@huawei.com>
2022-02-23 11:27:18 +0800

    add multi-user
    Signed-off-by: unknown <sijunjie@huawei.com>

--- services/abilitymgr/src/kernal_system_app_manager.cpp
+++ services/abilitymgr/src/kernal_system_app_manager.cpp
@@ -353,7 +353,7 @@ void KernalSystemAppManager::OnAbilityDied(std::shared_ptr<AbilityRecord> abilit
     abilityRecord->SetAbilityState(AbilityState::INITIAL);
     auto timeoutTask = [ams, abilityRecord]() {
         if (abilityRecord) {
-            ams->StartingSystemUiAbility(abilityRecord->GetAbilityInfo().name);
+            ams->StartingSystemUiAbility();
         }
     };
     handler->PostTask(timeoutTask, "SystemUi_Die_" + name, AbilityManagerService::RESTART_TIMEOUT);
@@ -387,7 +387,7 @@ void KernalSystemAppManager::OnTimeOut(uint32_t msgId, int64_t eventId)
             };
             handler->PostTask(task);
             auto timeoutTask = [abilityMS, name]() {
-                abilityMS->StartingSystemUiAbility(name);
+                abilityMS->StartingSystemUiAbility();
                 HILOG_ERROR("System UI on time out event: restart:%{public}s", name.c_str());
             };
             handler->PostTask(timeoutTask, "SystemUi_Timeout_" + name, AbilityManagerService::RESTART_TIMEOUT);
