6ac1302c9e9d50c23f12366aeaa2f49aae75b80d
njupthan <hanhaibin@huawei.com>
2022-02-28 19:38:50 +0800

    update code
    Signed-off-by: njupthan <hanhaibin@huawei.com>

--- services/appmgr/src/app_mgr_service_inner.cpp
+++ services/appmgr/src/app_mgr_service_inner.cpp
@@ -1907,7 +1907,7 @@ void AppMgrServiceInner::RestartResidentProcess(std::shared_ptr<AppRunningRecord
     }
     std::vector<BundleInfo> infos;
     infos.emplace_back(bundleInfo);
-    APP_LOGI("he resident process [%{public}s] remaining restarts num is [%{public}d]",
+    APP_LOGI("the resident process [%{public}s] remaining restarts num is [%{public}d]",
         appRecord->GetProcessName().c_str(), (int)appRecord->GetRestartResidentProcCount());
     StartResidentProcess(infos, appRecord->GetRestartResidentProcCount());
 }
