139d55992e8d46797f11a1bc6e90d03fb43eecba
altay <yuaqiang1@huawei.com>
2022-03-02 11:17:23 +0800

    fixed df93765 from https://gitee.com/altay/aafwk_standard/pulls/931 Description:fixed verifyUriPermission promise Sig:SIG_ApplicationFramework Feature or Bugfix:Bugfix Binary Source:No
    Signed-off-by: altay <yuaqiang1@huawei.com>
    Change-Id: I3303a6e68cbff6d1180b4fe714987d841ef6f0ee

--- interfaces/innerkits/uri_permission/src/uri_permission_manager_client.cpp
+++ interfaces/innerkits/uri_permission/src/uri_permission_manager_client.cpp
@@ -26,38 +26,33 @@ void UriPermissionManagerClient::GrantUriPermission(const Uri &uri, unsigned int
     const Security::AccessToken::AccessTokenID fromTokenId, const Security::AccessToken::AccessTokenID targetTokenId)
 {
     HILOG_DEBUG("UriPermissionManagerClient::GrantUriPermission is called.");
-    ErrCode ret = ConnectUriPermService();
-    if (ret != 0 || !uriPermMgr_) {
-        HILOG_ERROR("Failed to ConnectUriPermService");
-        return;
+    auto uriPermMgr = ConnectUriPermService();
+    if (uriPermMgr) {
+        uriPermMgr->GrantUriPermission(uri, flag, fromTokenId, targetTokenId);
     }
-    uriPermMgr_->GrantUriPermission(uri, flag, fromTokenId, targetTokenId);
 }
 
 bool UriPermissionManagerClient::VerifyUriPermission(const Uri &uri, unsigned int flag,
     const Security::AccessToken::AccessTokenID tokenId)
 {
     HILOG_DEBUG("UriPermissionManagerClient::VerifyUriPermission is called.");
-    ErrCode ret = ConnectUriPermService();
-    if (ret != 0 || !uriPermMgr_) {
-        HILOG_ERROR("Failed to ConnectUriPermService");
-        return false;
+    auto uriPermMgr = ConnectUriPermService();
+    if (uriPermMgr) {
+        return uriPermMgr->VerifyUriPermission(uri, flag, tokenId);
     }
-    return uriPermMgr_->VerifyUriPermission(uri, flag, tokenId);
+    return false;
 }
 
 void UriPermissionManagerClient::RemoveUriPermission(const Security::AccessToken::AccessTokenID tokenId)
 {
     HILOG_DEBUG("UriPermissionManagerClient::RemoveUriPermission is called.");
-    ErrCode ret = ConnectUriPermService();
-    if (ret != 0 || !uriPermMgr_) {
-        HILOG_ERROR("Failed to ConnectUriPermService");
-        return;
+    auto uriPermMgr = ConnectUriPermService();
+    if (uriPermMgr) {
+        uriPermMgr->RemoveUriPermission(tokenId);
     }
-    uriPermMgr_->RemoveUriPermission(tokenId);
 }
 
-ErrCode UriPermissionManagerClient::ConnectUriPermService()
+sptr<IUriPermissionManager> UriPermissionManagerClient::ConnectUriPermService()
 {
     HILOG_DEBUG("UriPermissionManagerClient::ConnectUriPermService is called.");
     std::lock_guard<std::mutex> lock(mutex_);
@@ -65,25 +60,38 @@ ErrCode UriPermissionManagerClient::ConnectUriPermService()
         auto systemAbilityMgr = SystemAbilityManagerClient::GetInstance().GetSystemAbilityManager();
         if (!systemAbilityMgr) {
             HILOG_ERROR("Failed to get SystemAbilityManager.");
-            return -1;
+            return nullptr;
         }
 
         auto remoteObj = systemAbilityMgr->GetSystemAbility(URI_PERMISSION_MGR_SERVICE_ID);
         if (!remoteObj || (uriPermMgr_ = iface_cast<IUriPermissionManager>(remoteObj)) == nullptr) {
             HILOG_ERROR("Failed to get UriPermService.");
-            return -1;
+            return nullptr;
         }
+        auto self = shared_from_this();
+        const auto& onClearProxyCallback = [self] {
+            if (self) {
+                self->ClearProxy();
+            }
+        };
+        sptr<UpmsDeathRecipient> recipient(new UpmsDeathRecipient(onClearProxyCallback));
+        uriPermMgr_->AsObject()->AddDeathRecipient(recipient);
     }
-    sptr<UpmsDeathRecipient> recipient(new UpmsDeathRecipient(uriPermMgr_));
-    uriPermMgr_->AsObject()->AddDeathRecipient(recipient);
     HILOG_DEBUG("End UriPermissionManagerClient::ConnectUriPermService.");
-    return 0;
+    return uriPermMgr_;
+}
+
+void UriPermissionManagerClient::ClearProxy()
+{
+    HILOG_DEBUG("UriPermissionManagerClient::ClearProxy is called.");
+    std::lock_guard<std::mutex> lock(mutex_);
+    uriPermMgr_ = nullptr;
 }
 
 void UriPermissionManagerClient::UpmsDeathRecipient::OnRemoteDied([[maybe_unused]] const wptr<IRemoteObject>& remote)
 {
     HILOG_ERROR("upms stub died.");
-    proxy_ = nullptr;
+    proxy_();
 }
 }  // namespace AAFwk
 }  // namespace OHOS
