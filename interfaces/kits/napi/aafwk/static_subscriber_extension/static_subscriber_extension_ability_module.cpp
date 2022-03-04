385d04106fd117f637e0c6c185c0b8f8ae47c0be
liqiang <liqiang121@huawei.com>
2022-03-04 09:13:29 +0800

    IssueNo:https://gitee.com/openharmony/aafwk_standard/issues/I4W8M7 Description:change StaticSubscriberExtension to StaticSubscriberExtensionAbility Sig: SIG_ApplicationFramework Feature or Bugfix:Bugfix Binary Source:No
    Signed-off-by: liqiang <liqiang121@huawei.com>
    Change-Id: I548739fb8f5b0eaec8becd01675513acde15de6f

---
+++ interfaces/kits/napi/aafwk/static_subscriber_extension/static_subscriber_extension_ability_module.cpp
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "native_engine/native_engine.h"
+
+extern const char _binary_static_subscriber_extension_ability_js_start[];
+extern const char _binary_static_subscriber_extension_ability_js_end[];
+extern const char _binary_static_subscriber_extension_ability_abc_start[];
+extern const char _binary_static_subscriber_extension_ability_abc_end[];
+
+extern "C" __attribute__((constructor))
+void NAPI_application_StaticSubscriberExtensionAbility_AutoRegister()
+{
+    auto moduleManager = NativeModuleManager::GetInstance();
+    NativeModule newModuleInfo = {
+        .name = "application.StaticSubscriberExtensionAbility",
+        .fileName = "application/libstaticsubscriberextension_napi.so/StaticSubscriberExtensionAbility.js",
+    };
+
+    moduleManager->Register(&newModuleInfo);
+}
+
+extern "C" __attribute__((visibility("default")))
+void NAPI_application_StaticSubscriberExtensionAbility_GetJSCode(const char **buf, int *bufLen)
+{
+    if (buf != nullptr) {
+        *buf = _binary_static_subscriber_extension_ability_js_start;
+    }
+
+    if (bufLen != nullptr) {
+        *bufLen = _binary_static_subscriber_extension_ability_js_end -
+            _binary_static_subscriber_extension_ability_js_start;
+    }
+}
+
+// StaticSubscriberExtensionAbility JS register
+extern "C" __attribute__((visibility("default")))
+void NAPI_application_StaticSubscriberExtensionAbility_GetABCCode(const char **buf, int *buflen)
+{
+    if (buf != nullptr) {
+        *buf = _binary_static_subscriber_extension_ability_abc_start;
+    }
+    if (buflen != nullptr) {
+        *buflen = _binary_static_subscriber_extension_ability_abc_end -
+            _binary_static_subscriber_extension_ability_abc_start;
+    }
+}
