7d128103851e375e883d2071b180987ff7ac2466
jsjzju <jinsenjun@huawei.com>
2022-03-04 15:42:06 +0800

    IssueNo:#I4W01I Description: Fix crash caused by StopUser Sig:SIG_ApplicationFramework Feature or Bugfix:Bugfix Binary Source:No
    Signed-off-by: jsj <jinsenjun@huawei.com>
    Change-Id: Ib371000c6d86a4ad9fb4cd5af404f21976dab110

--- services/abilitymgr/include/ability_manager_service.h
+++ services/abilitymgr/include/ability_manager_service.h
@@ -17,6 +17,7 @@
 #define OHOS_AAFWK_ABILITY_MANAGER_SERVICE_H
 
 #include <memory>
+#include <shared_mutex>
 #include <singleton.h>
 #include <thread_ex.h>
 #include <unordered_map>
@@ -1180,6 +1181,7 @@ private:
     sptr<AppExecFwk::IAbilityController> abilityController_ = nullptr;
     bool controllerIsAStabilityTest_ = false;
     std::recursive_mutex globalLock_;
+    std::shared_mutex managersMutex_;
 
     std::multimap<std::string, std::string> timeoutMap_;
 };
