09e27b0309c722f38e11ad8b83df5d50b3feb438
zhongjianfei <zhongjianfei@huawei.com>
2022-03-14 11:07:09 +0800

    fix ReadUnsupportedData bugs
    Signed-off-by: zhongjianfei <zhongjianfei@huawei.com>
    Change-Id: Ib0ae8bc937b2218688aae184a505f60b43a56953

--- frameworks/kits/content/cpp/src/ohos/aafwk/content/want_params.cpp
+++ frameworks/kits/content/cpp/src/ohos/aafwk/content/want_params.cpp
@@ -1190,11 +1190,13 @@ bool WantParams::ReadFromParcelDouble(Parcel &parcel, const std::string &key)
 
 bool WantParams::ReadUnsupportedData(Parcel &parcel, const std::string &key, int type)
 {
-    int bufferSize = 0;
+    int32_t bufferSize = 0;
     if (!parcel.ReadInt32(bufferSize)) {
         return false;
     }
-    if (bufferSize < 0) {
+    static constexpr int32_t maxAllowedSize = 100 * 1024 * 1024;
+    if (bufferSize < 0 || bufferSize > maxAllowedSize) {
+        ABILITYBASE_LOGE("%{public}s invalid size: %{public}d", __func__, bufferSize);
         return false;
     }
 
