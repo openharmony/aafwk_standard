147683d1d2b6f6d94f97cec9ee548515a0771ca0
jsjzju <jinsenjun@huawei.com>
2022-03-16 12:10:45 +0800

    fixed 1e3c52f from https://gitee.com/jsjzju/aafwk_standard/pulls/1228 IssueNo:#I4Y2D8 Description: Fix the memory leak of ReadParcelable Sig:SIG_ApplicationFramework Feature or Bugfix:Bugfix Binary Source:No
    Signed-off-by: jsjzju <jinsenjun@huawei.com>
    Change-Id: Iddf80da6bccf72e24b548905a0275d342cc7e3ad

--- frameworks/kits/ability/native/src/datashare_stub.cpp
+++ frameworks/kits/ability/native/src/datashare_stub.cpp
@@ -265,7 +265,7 @@ ErrCode DataShareStub::CmdBatchInsert(MessageParcel &data, MessageParcel &reply)
 
     std::vector<NativeRdb::ValuesBucket> values;
     for (int i = 0; i < count; i++) {
-        NativeRdb::ValuesBucket *value = data.ReadParcelable<NativeRdb::ValuesBucket>();
+        std::unique_ptr<NativeRdb::ValuesBucket> value(data.ReadParcelable<NativeRdb::ValuesBucket>());
         if (value == nullptr) {
             HILOG_ERROR("DataShareStub value is nullptr, index = %{public}d", i);
             return ERR_INVALID_VALUE;
