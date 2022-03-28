deec57bd6f1b14f2953e58367d0fc94a1761274b
jiangdayuan <jiangdayuan@huawei.com>
2022-03-28 20:19:56 +0800

    add api7 support window title
    Signed-off-by: jiangdayuan <jiangdayuan@huawei.com>

--- frameworks/kits/ability/native/src/page_ability_impl.cpp
+++ frameworks/kits/ability/native/src/page_ability_impl.cpp
@@ -217,9 +217,9 @@ void PageAbilityImpl::DoKeyDown(const std::shared_ptr<MMI::KeyEvent>& keyEvent)
         HILOG_ERROR("PageAbilityImpl::DoKeyDown ability_ == nullptr");
         return;
     }
-    auto abilitInfo = ability_->GetAbilityInfo();
+    auto abilityInfo = ability_->GetAbilityInfo();
     HILOG_INFO("PageAbilityImpl::DoKeyDown called %{public}s And Focus is %{public}s",
-        abilitInfo->name.c_str(),
+        abilityInfo->name.c_str(),
         ability_->HasWindowFocus() ? "true" : "false");
 
     ability_->OnKeyDown(keyEvent);
@@ -241,9 +241,9 @@ void PageAbilityImpl::DoKeyUp(const std::shared_ptr<MMI::KeyEvent>& keyEvent)
         HILOG_ERROR("PageAbilityImpl::DoKeyUp ability_ == nullptr");
         return;
     }
-    auto abilitInfo = ability_->GetAbilityInfo();
+    auto abilityInfo = ability_->GetAbilityInfo();
     HILOG_INFO("PageAbilityImpl::DoKeyUp called %{public}s And Focus is %{public}s",
-        abilitInfo->name.c_str(),
+        abilityInfo->name.c_str(),
         ability_->HasWindowFocus() ? "true" : "false");
 
     ability_->OnKeyUp(keyEvent);
@@ -265,9 +265,9 @@ void PageAbilityImpl::DoPointerEvent(std::shared_ptr<MMI::PointerEvent>& pointer
         HILOG_ERROR("PageAbilityImpl::DoPointerEvent ability_ == nullptr");
         return;
     }
-    auto abilitInfo = ability_->GetAbilityInfo();
+    auto abilityInfo = ability_->GetAbilityInfo();
     HILOG_INFO("PageAbilityImpl::DoPointerEvent called %{public}s And Focus is %{public}s",
-        abilitInfo->name.c_str(),
+        abilityInfo->name.c_str(),
         ability_->HasWindowFocus() ? "true" : "false");
 
     ability_->OnPointerEvent(pointerEvent);
