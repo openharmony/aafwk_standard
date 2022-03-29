04a8c054cfab86c871b026ae1980e94a13d5c33d
jsjzju <jinsenjun@huawei.com>
2022-03-29 16:55:00 +0800

    IssueNo:#I505MN Description: Function returns a reference to a local variable Sig:SIG_ApplicationFramework Feature or Bugfix:Bugfix Binary Source:No
    Signed-off-by: jsjzju <jinsenjun@huawei.com>
    Change-Id: I30f44f6bfd1a4e87f3c3ce3359d75e23310bf953

--- services/abilitymgr/src/ability_manager_service.cpp
+++ services/abilitymgr/src/ability_manager_service.cpp
@@ -2931,7 +2931,7 @@ bool AbilityManagerService::VerificationAllToken(const sptr<IRemoteObject> &toke
     return false;
 }
 
-const std::shared_ptr<DataAbilityManager> &AbilityManagerService::GetDataAbilityManager(
+std::shared_ptr<DataAbilityManager> AbilityManagerService::GetDataAbilityManager(
     const sptr<IAbilityScheduler> &scheduler)
 {
     if (scheduler == nullptr) {
@@ -2940,7 +2940,7 @@ const std::shared_ptr<DataAbilityManager> &AbilityManagerService::GetDataAbility
     }
 
     std::shared_lock<std::shared_mutex> lock(managersMutex_);
-    for (auto item: dataAbilityManagers_) {
+    for (auto& item: dataAbilityManagers_) {
         if (item.second && item.second->ContainsDataAbility(scheduler)) {
             return item.second;
         }
