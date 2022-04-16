e4166a2cb326b36802bdc3432b3a6b7cc7e2d02c
caochunlei <caochunlei1@huawei.com>
2022-04-16 10:20:21 +0800

    caochunlei1@huawei.com
    Signed-off-by: caochunlei <caochunlei1@huawei.com>

--- interfaces/kits/napi/aafwk/ability_context/ability_context.js
+++ interfaces/kits/napi/aafwk/ability_context/ability_context.js
@@ -92,6 +92,14 @@ class AbilityContext extends Context {
     restoreWindowStage(contentStorage) {
         return this.__context_impl__.restoreWindowStage(contentStorage)
     }
+
+    setMissionLabel(label) {
+        return this.__context_impl__.setMissionLabel(label)
+    }
+
+    setMissionIcon(icon) {
+        return this.__context_impl__.setMissionIcon(icon)
+    }
 }
 
 export default AbilityContext
