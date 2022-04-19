01b531e5253e276aec98ba5da3fc017f80c780a8
xuchenghua09 <xuchenghua09@huawei.com>
2022-04-19 16:22:29 +0000

    Fix codex
    Signed-off-by: xuchenghua09 <xuchenghua09@huawei.com>

--- idl/test/ts/common/idl_common.h
+++ idl/test/ts/common/idl_common.h
@@ -138,7 +138,10 @@ public:
         for (int i = 0; i < argc_; ++i) {
             const int itemSize = strlen(args[i]);
             argv_[i] = new char[itemSize + 1] {0};
-            (void)strcpy_s(argv_[i], itemSize + 1, args[i]);
+            auto ret = strcpy_s(argv_[i], itemSize + 1, args[i]);
+            if (ret != EOK) {
+                GTEST_LOG_(ERROR) << "strcpy_s error [" << ret << "]";
+            }
         }
     };
     ~ParameterArgv()
