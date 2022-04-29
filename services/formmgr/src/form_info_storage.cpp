fdcde02694b005731a15da5cbf1ca17c83786efb
liweifeng <liweifeng2@huawei.com>
2022-04-29 15:23:04 +0800

    解决卡片多用户安装、卸载问题
    Signed-off-by: liweifeng <liweifeng2@huawei.com>
    Change-Id: Ie329794720f772308b4384fe0f37983c0ae04661

--- services/formmgr/src/form_info_storage.cpp
+++ services/formmgr/src/form_info_storage.cpp
@@ -14,27 +14,61 @@
  */
 
 #include "form_info_storage.h"
+
+#include "hilog_wrapper.h"
 #include "json_serializer.h"
 
 namespace OHOS {
 namespace AAFwk {
 namespace {
 const std::string JSON_KEY_USER_ID = "userId";
-const std::string JSON_KEY_FORM_INFO = "formInfo";
+const std::string JSON_KEY_FORM_INFO = "formInfos";
 } // namespace
+
+FormInfoStorage::FormInfoStorage(int32_t userId, std::vector<AppExecFwk::FormInfo> &formInfos)
+{
+    this->userId = userId;
+    for (const auto &item : formInfos) {
+        this->formInfos.push_back(item);
+    }
+}
+
+void FormInfoStorage::GetAllFormsInfo(int32_t userId, std::vector<AppExecFwk::FormInfo> &formInfos) const
+{
+    HILOG_INFO("Get all forms infos, current userId is%{public}d, this userId is %{public}d.", userId, this->userId);
+    if (this->userId != userId) {
+        return;
+    }
+    for (const auto &item : this->formInfos) {
+        formInfos.push_back(item);
+    }
+}
+
+void FormInfoStorage::GetFormsInfoByModule(int32_t userId, const std::string &moduleName,
+    std::vector<AppExecFwk::FormInfo> &formInfos) const
+{
+    if (this->userId != userId) {
+        return;
+    }
+    for (const auto &item : this->formInfos) {
+        if (item.moduleName == moduleName) {
+            formInfos.push_back(item);
+        }
+    }
+}
 
 void to_json(nlohmann::json &jsonObject, const FormInfoStorage &formInfoStorage)
 {
     jsonObject = nlohmann::json {
         {JSON_KEY_USER_ID, formInfoStorage.userId},
-        {JSON_KEY_FORM_INFO, formInfoStorage.formInfo}
+        {JSON_KEY_FORM_INFO, formInfoStorage.formInfos}
     };
 }
 
 void from_json(const nlohmann::json &jsonObject, FormInfoStorage &formInfoStorage)
 {
     formInfoStorage.userId = jsonObject.at(JSON_KEY_USER_ID).get<int32_t>();
-    formInfoStorage.formInfo = jsonObject.at(JSON_KEY_FORM_INFO).get<AppExecFwk::FormInfo>();
+    formInfoStorage.formInfos = jsonObject.at(JSON_KEY_FORM_INFO).get<std::vector<AppExecFwk::FormInfo>>();
 }
 } // namespace AppExecFwk
 } // namespace OHOS
