b3871d38059f7d19204b60be4a699d175b4fd459
linjun9528 <linjun27@huawei.com>
2022-05-06 15:42:47 +0800

    add file extension code
    Signed-off-by: linjun9528 <linjun27@huawei.com>

--- frameworks/kits/ability/native/src/ability_thread.cpp
+++ frameworks/kits/ability/native/src/ability_thread.cpp
@@ -58,6 +58,7 @@ constexpr static char DATA_SHARE_EXT_ABILITY[] = "DataShareExtAbility";
 constexpr static char WORK_SCHEDULER_EXTENSION[] = "WorkSchedulerExtension";
 constexpr static char ACCESSIBILITY_EXTENSION[] = "AccessibilityExtension";
 constexpr static char WALLPAPER_EXTENSION[] = "WallpaperExtension";
+constexpr static char FILE_EXT_ABILITY[] = "FileExtAbility";
 
 /**
  * @brief Default constructor used to create a AbilityThread instance.
@@ -138,6 +139,9 @@ std::string AbilityThread::CreateAbilityName(const std::shared_ptr<AbilityLocalR
         }
         if (abilityInfo->extensionAbilityType == ExtensionAbilityType::WALLPAPER) {
             abilityName = WALLPAPER_EXTENSION;
+        }
+        if (abilityInfo->extensionAbilityType == ExtensionAbilityType::FILE) {
+            abilityName = FILE_EXT_ABILITY;
         }
         HILOG_INFO("CreateAbilityName extension type, abilityName:%{public}s", abilityName.c_str());
     } else {
