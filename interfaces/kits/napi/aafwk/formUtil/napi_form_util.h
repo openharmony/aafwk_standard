bbf6fc25a517fa2f3e1ff13edb26246aec9e9627
liweifeng <liweifeng2@huawei.com>
2022-05-07 10:50:03 +0800

    fix enum value
    Signed-off-by: liweifeng <liweifeng2@huawei.com>
    Change-Id: I96337dca6ca70cd71e1282aa0c7210fccc37148d

--- interfaces/kits/napi/aafwk/formUtil/napi_form_util.h
+++ interfaces/kits/napi/aafwk/formUtil/napi_form_util.h
@@ -22,6 +22,29 @@
 #include "napi/native_node_api.h"
 #include "nlohmann/json.hpp"
 
+const int32_t ERR_COMMON = 1;
+const int32_t ERR_PERMISSION_DENY = 2;
+const int32_t ERR_GET_INFO_FAILED = 4;
+const int32_t ERR_GET_BUNDLE_FAILED = 5;
+const int32_t ERR_GET_LAYOUT_FAILED = 6;
+const int32_t ERR_ADD_INVALID_PARAM = 7;
+const int32_t ERR_CFG_NOT_MATCH_ID = 8;
+const int32_t ERR_NOT_EXIST_ID = 9;
+const int32_t ERR_BIND_PROVIDER_FAILED = 10;
+const int32_t ERR_MAX_SYSTEM_FORMS = 11;
+const int32_t ERR_MAX_INSTANCES_PER_FORM = 12;
+const int32_t ERR_OPERATION_FORM_NOT_SELF = 13;
+const int32_t ERR_PROVIDER_DEL_FAIL = 14;
+const int32_t ERR_MAX_FORMS_PER_CLIENT = 15;
+const int32_t ERR_MAX_SYSTEM_TEMP_FORMS = 16;
+const int32_t ERR_FORM_NO_SUCH_MODULE = 17;
+const int32_t ERR_FORM_NO_SUCH_ABILITY = 18;
+const int32_t ERR_FORM_NO_SUCH_DIMENSION = 19;
+const int32_t ERR_FORM_FA_NOT_INSTALLED = 20;
+const int32_t ERR_SYSTEM_RESPONSES_FAILED = 30;
+const int32_t ERR_FORM_DUPLICATE_ADDED = 31;
+const int32_t ERR_IN_RECOVERY = 36;
+
 struct AsyncErrMsgCallbackInfo {
     napi_env env;
     napi_async_work asyncWork;
