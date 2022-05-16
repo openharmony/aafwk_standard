4661e990b904c33f616085c36af720f893a78736
mingxihua <mingxihua@huawei.com>
2022-05-16 17:29:55 +0800

    mingxihua@huawei.com.cn
    Signed-off-by: mingxihua <mingxihua@huawei.com>

--- frameworks/kits/appkit/native/ability_runtime/context/context_impl.cpp
+++ frameworks/kits/appkit/native/ability_runtime/context/context_impl.cpp
@@ -54,9 +54,9 @@ const std::string ContextImpl::CONTEXT_TEMP("/temp");
 const std::string ContextImpl::CONTEXT_FILES("/files");
 const std::string ContextImpl::CONTEXT_HAPS("/haps");
 const std::string ContextImpl::CONTEXT_ELS[] = {"el1", "el2"};
+const int32_t DEFAULT_EL = 1;
 #ifndef OS_ACCOUNT_PART_ENABLED
 const int32_t DEFAULT_OS_ACCOUNT_ID = 0; // 0 is the default id when there is no os_account part
-const int32_t DEFAULT_EL = 1;
 #endif // OS_ACCOUNT_PART_ENABLED
 
 std::string ContextImpl::GetBundleName() const
