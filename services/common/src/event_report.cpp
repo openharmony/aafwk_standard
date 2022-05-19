cba497e4e3b5e5cfa84e8cf2172fa0a6c18ecc6f
zhoushicheng2@huawei.com <zhoushicheng2@huawei.com>
2022-05-19 10:41:50 +0800

    zhoushicheng2@huawei.com
    Signed-off-by: zhoushicheng2@huawei.com <zhoushicheng2@huawei.com>
    Change-Id: Iba66fa46a88c68570ac4cfeacd575a508b6dbdd1

---
+++ services/common/src/event_report.cpp
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2022-2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "event_report.h"
+#include "hisysevent.h"
+#include "hilog_wrapper.h"
+
+#include <chrono>
+
+namespace OHOS {
+namespace AAFWK {
+namespace {
+const std::string EVENT_KEY_PID = "PID";
+const std::string EVENT_KEY_UID = "UID";
+const std::string EVENT_KEY_RID = "RID";
+const std::string EVENT_KEY_ABILITY_NAME = "ABILITY_NAME";
+const std::string EVENT_KEY_ABILITY_TYPE = "ABILITY_TYPE";
+const std::string EVENT_KEY_MODEL_TYPE = "MODEL_TYPE";
+const std::string EVENT_KEY_BUNDLE_NAME = "BUNDLE_NAME";
+const std::string EVENT_KEY_DEVICEID = "DEVICEID";
+const std::string EVENT_KEY_URI = "URI";
+const std::string EVENT_KEY_ACTION = "ACTION";
+const std::string EVENT_KEY_APP_NAME = "NAME";
+const std::string EVENT_KEY_VERSION_NAME = "VERSION_NAME";
+const std::string EVENT_KEY_VERSION_CODE = "VERSION_CODE";
+const std::string EVENT_KEY_TIME_STAMP = "TIME_STAMP";
+const std::string TYPE = "TYPE";
+}
+void EventReport::SendHiSysEvent(const std::string &abilityName, const std::string &eventName,
+    HiSysEventType type)
+{
+    EventReport::EventWrite(
+        eventName,
+        type,
+        EVENT_KEY_ABILITY_NAME, abilityName);
+    HILOG_WARN("{eventName}: abilityName: %{public}s",
+        abilityName.c_str());
+}
+// void EventReport::AppEvent(const std::shared_ptr<ApplicationInfo> &applicationInfo,
+//     const std::string &pid, const std::string &eventName, HiSysEventType type)
+// {
+//     std::string name = applicationInfo->name.c_str();
+//     std::string versionName = applicationInfo->versionName.c_str();
+//     uint32_t versionCode = applicationInfo->versionCode;
+//     int32_t timeStamp =
+//         std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now().time_since_epoch())
+//         .count();
+//     EventReport::EventWrite(
+//         eventName,
+//         type,
+//         EVENT_KEY_APP_NAME, name,
+//         EVENT_KEY_VERSION_NAME, versionName,
+//         EVENT_KEY_VERSION_CODE, std::to_string(versionCode),
+//         EVENT_KEY_PID, pid.c_str(),
+//         EVENT_KEY_TIME_STAMP, std::to_string(timeStamp));
+//     HILOG_WARN("{eventName}: name: %{public}s, versionName: %{public}s,"
+//         "versionCode: %{public}d, pid: %{public}s, timeStamp: %{public}d",
+//         name.c_str(),
+//         versionName.c_str(),
+//         versionCode,
+//         pid.c_str(),
+//         timeStamp);
+// }
+template<typename... Types>
+void EventReport::EventWrite(
+    const std::string &eventName,
+    HiSysEventType type,
+    Types... keyValues)
+{
+    OHOS::HiviewDFX::HiSysEvent::Write(
+        OHOS::HiviewDFX::HiSysEvent::Domain::AAFWK,
+        eventName,
+        static_cast<OHOS::HiviewDFX::HiSysEvent::EventType>(type),
+        keyValues...);
+}
+}  // namespace AAFWK
+}  // namespace OHOS
